e8df76a59346ac17be75071f1fd1351e917ce217
Andrei Branza <andrei.branza@yahoo.com>
2024-05-20 17:19:06 +0300

    more logic to display annual results

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import UserInput from "./components/UserInput";
 import Result from './components/Result'
+import { useState } from "react"
 
 function App() {
 
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+  
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(prevUserInput => {
+        return {
+            ...prevUserInput,
+            [inputIdentifier]: +newValue
+        }
+    })
+}
+
   return (
     <>
-      <UserInput />
-      <Result /> 
+      <UserInput onChange={handleChange} userInput = {userInput} />
+      <Result userInput = {userInput} /> 
     </>   
   )
 }
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,14 +1,11 @@
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults } from "../util/investment.js";
+import { formatter } from "../util/investment.js";
 
-export default function Result({ initialInvestment,
-    annualInvestment,
-    expectedReturn,
-    duration}) {
+export default function Result({userInput}) {
 
-    const annualData = calculateInvestmentResults({ initialInvestment,
-        annualInvestment,
-        expectedReturn,
-        duration});
+    const annualData = calculateInvestmentResults(userInput);
+    const initialInvestment = annualData[0].valueEndOfYear - annualData[0].interest - annualData[0].annualInvestment
+
     return(
         <table id='result'>           
             <thead>
@@ -21,10 +18,20 @@ export default function Result({ initialInvestment,
                 </tr>
             </thead>
             <tbody>
-                {/* aici tre sa fac un for each sa imi afiseze cate un tablerow pt fiecare element din annual data */}
-               {annualData.forEach((annualResult => {
-                <TableRow result = {annualResult}/>
-               }))}                
+               {annualData.map(annualResult => 
+                    {
+                        const totalInterest = annualResult.valueEndOfYear - annualResult.annualInvestment * annualResult.year - initialInvestment
+                        const totalAmountInvested = annualResult.valueEndOfYear - totalInterest
+                        return( 
+                            <tr key={annualResult.year}> 
+                                <td>{annualResult.year}</td>
+                                <td>{formatter.format(annualResult.valueEndOfYear)}</td>
+                                <td>{formatter.format(annualResult.interest)}</td>
+                                <td>{formatter.format(totalInterest)}</td>
+                                <td>{formatter.format(totalAmountInvested)}</td>
+                            </tr>)
+                    })
+                }           
             </tbody>
         </table>
     
