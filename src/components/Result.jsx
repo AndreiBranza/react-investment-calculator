cb0a9e8fe2ffcb7f5bcac05c4dfc0d976a03d030
Andrei Branza <andrei.branza@yahoo.com>
2024-05-20 12:57:37 +0300

    Added the result formatting

---
+++ src/components/Result.jsx
@@ -0,0 +1,17 @@
+export default function Restul() {
+    return(
+        <table id='result'>           
+            <thead>
+                <th>Year</th>
+                <th>Investment Value</th>
+                <th>Interest (Year)</th>
+                <th>Total Interest</th>
+                <th>Invested Capital</th>
+            </thead>
+            <tbody>
+                
+            </tbody>
+        </table>
+    
+    )
+}
